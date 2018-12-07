d1f7b810dc4ef485e732aabe9484c8ac0cc4ef78
mbwatson <mattwt@gmail.com>
2018-12-06 19:09:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const PORT = 3000
+
+app.use(bodyParser.json())
+app.listen(PORT)
+
+app.get('/', (req, res, next) => {
+    res.send('Hello!')
+})
