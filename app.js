c8c23c0a433849c30994e1d0b08a78ebaa8f5fee
mbwatson <mattwat@gmail.com>
2018-12-19 18:43:08 -0500

    add schema

--- app.js
+++ app.js
@@ -6,30 +6,54 @@ const { buildSchema } = require('graphql')
 const app = express()
 const PORT = 3000
 
+const events = []
+
 app.use(bodyParser.json())
 
 app.use('/graphql', graphQlHttp({
     schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
-            query: RootQuery,
-            mutation: RootMutation,
+            query: RootQuery
+            mutation: RootMutation
         }
-    `),
+     `),
     rootValue: {
         events: () => {
-            return ['Cooking', 'Sailing', 'Coding']
+            return events
         },
         createEvent: (args) => {
-            const eventName = args.name
-            return eventName
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+            events.push(event)
+            return event
         }
     },
     graphiql: true,
