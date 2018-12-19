356ba6ff96892c3ef5d2e512abef12d75b307f57
mbwatson <mattwat@gmail.com>
2018-12-18 20:57:07 -0500

    add graphql

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphQlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 const PORT = 3000
 
 app.use(bodyParser.json())
+
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery,
+            mutation: RootMutation,
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Cooking', 'Sailing', 'Coding']
+        },
+        createEvent: (args) => {
+            const eventName = args.name
+            return eventName
+        }
+    },
+    graphiql: true,
+}))
+
+//
+// In GraphiQL interface...
+//
+// localhost:3000/graphql
+//
+// query {
+//   events
+// }
+//
+// mutation {
+//   createEvent(name: "Sports")
+// }
+//
+
 app.listen(PORT)
 
-app.get('/', (req, res, next) => {
-    res.send('Hello!')
-})
